771fb291d1b906cbae1787b0d4a30dd6b4429cc7
Filip Hustava <filipchaser@gmail.com>
2024-07-12 11:37:29 +0200

    Filip 004
    TBR Place -generate_custom_prompt (with  messages)
    views.py, basic settings to AWS use amazon API for BOOKS

--- tbr_place/views.py
+++ tbr_place/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import Prompt
+from .models import Prompt, MyPrompt
 # Create your views here.
 import random
 from django.contrib import messages
@@ -29,3 +29,40 @@ def generate_random_prompt(request):
         messages.warning(request, str(e))
 
     return render(request, 'index.html', context)
+
+
+
+def generate_custom_prompt(request):
+    """
+        Generovanie vlastného náhodného promptu.
+    """
+    if request.method == 'POST':
+        selected_types = request.POST.getlist('selectedTypes[]')
+
+        # Vygenerovanie promptu na základe vybraných typov
+        generated_prompt = "Here is your custom prompt: "
+
+        prompts = MyPrompt.objects.filter(prompt_type__myprompt_type_name__in=selected_types)
+
+        if prompts.exists():
+            random_prompt = random.choice(prompts)
+            generated_prompt += random_prompt.prompt_name
+            messages.success(request, 'Custom prompt successfully generated!')
+        else:
+            generated_prompt += "No prompt available for selected types."
+            messages.warning(request, 'No prompts available for selected types.')
+
+        return render(request, 'index.html', {'generated_prompt': generated_prompt})
+
+    return render(request, 'index.html')
+
+
+def search_book(request):
+    if request.method == 'POST':
+        isbn = request.POST.get('isbn')
+        # book_details = get_book_details(isbn)
+
+# todo - get aws acc
+
+    #     return render(request, 'index.html', {'book_details': book_details})
+    # return render(request, 'index.html')
