10b1931a5972357ac69cf042c228b5886c7be465
okriz <kriz111@gmail.com>
2024-07-20 11:44:05 +0200

    new start2

--- tbr_place_project/urls.py
+++ tbr_place_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views as account_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('', account_views.generate_random_prompt, name='index'),  # Ukazuje na váš view pro generování promptu
+    path('accounts/', include('accounts.urls')),
 ]
