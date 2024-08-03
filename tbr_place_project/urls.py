b27e8e80c0d59dbfaee4e2cabd1b9f5823a4a054
okriz <kriz111@gmail.com>
2024-08-03 09:48:36 +0200

    Ondra 1 opravena verze, bez index

--- tbr_place_project/urls.py
+++ tbr_place_project/urls.py
@@ -1,8 +1,37 @@
+"""
+URL configuration for tbr_place_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import include
 from django.contrib import admin
-from django.urls import path, include
-from accounts import views as accounts_views
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+from tbr_place import utils
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-]
+                  path("admin/", admin.site.urls),
+                  path('accounts/', include('accounts.urls')),
+                  path('', include('tbr_place.urls')),
+                  # path('literary-works/', include('literary_works.urls')),
+                  # path('api/', include('literary_works.urls')),
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
