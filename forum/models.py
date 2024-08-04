df3d331efd23bcd25c0a40714e3bd0b7129049db
okriz <kriz111@gmail.com>
2024-08-04 12:27:32 +0200

    fungujici forum

--- forum/models.py
+++ forum/models.py
@@ -2,31 +2,33 @@ from django.db import models
 from django.conf import settings
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 class Subcategory(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class Thread(models.Model):
-    title = models.CharField(max_length=100)
-    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()  # Ujistěte se, že tento řádek je přítomen
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 class Post(models.Model):
-    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
     content = models.TextField()
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.content
+        return self.content[:20]
--- forum/views.py
+++ forum/views.py
@@ -1,75 +1,40 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Category, Subcategory, Thread, Post
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from .models import Thread, Post, Subcategory
+from .forms import ThreadForm, PostForm
 
-def test_view(request):
-    return HttpResponse("Test view is working.")
-
+@login_required
 def forum_home_view(request):
-    categories = Category.objects.all()
-    return render(request, 'forum/forum_home.html', {'categories': categories})
+    threads = Thread.objects.all()
+    user_threads = Thread.objects.filter(author=request.user)
+    subcategories = Subcategory.objects.all()
+    return render(request, 'forum/forum_home.html', {'threads': threads, 'user_threads': user_threads, 'subcategories': subcategories})
 
-def category_detail_view(request, pk):
-    category = get_object_or_404(Category, pk=pk)
-    subcategories = category.subcategory_set.all()
-    return render(request, 'forum/category_detail.html', {'category': category, 'subcategories': subcategories})
-
-def subcategory_detail_view(request, pk):
-    subcategory = get_object_or_404(Subcategory, pk=pk)
-    threads = subcategory.thread_set.all()
-    return render(request, 'forum/subcategory_detail.html', {'subcategory': subcategory, 'threads': threads})
-
+@login_required
 def thread_detail_view(request, pk):
     thread = get_object_or_404(Thread, pk=pk)
-    posts = thread.post_set.all()
-    return render(request, 'forum/thread_detail.html', {'thread': thread, 'posts': posts})
+    posts = Post.objects.filter(thread=thread)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.thread = thread
+            post.author = request.user
+            post.save()
+            return redirect('thread_detail', pk=thread.pk)
+    else:
+        form = PostForm()
+    return render(request, 'forum/thread_detail.html', {'thread': thread, 'posts': posts, 'form': form})
 
 @login_required
 def create_thread_view(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        subcategory_id = request.POST['subcategory_id']
-        subcategory = get_object_or_404(Subcategory, pk=subcategory_id)
-        thread = Thread.objects.create(title=title, subcategory=subcategory, author=request.user)
-        return redirect('thread_detail', pk=thread.pk)
-    subcategories = Subcategory.objects.all()
-    return render(request, 'forum/create_thread.html', {'subcategories': subcategories})
-
-@login_required
-def edit_post_view(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
-        post.content = request.POST['content']
-        post.save()
-        return redirect('thread_detail', pk=post.thread.pk)
-    return render(request, 'forum/edit_post.html', {'post': post})
-
-@login_required
-def delete_post_view(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    thread_pk = post.thread.pk
-    post.delete()
-    return redirect('thread_detail', pk=thread_pk)
-
-@login_required
-def vote_post_view(request, pk, vote_type):
-    post = get_object_or_404(Post, pk=pk)
-    # Přidejte logiku pro hlasování
-    return redirect('thread_detail', pk=post.thread.pk)
-
-@login_required
-def report_post_view(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    # Přidejte logiku pro reportování
-    return redirect('thread_detail', pk=post.thread.pk)
-
-
-def title__icontains(query):
-    pass
-
-
-def search_results_view(request):
-    query = request.GET.get('q')
-    threads = Thread.objects.filter(title__icontains(query))
-    return render(request, 'forum/search_results.html', {'threads': threads})
+        form = ThreadForm(request.POST)
+        if form.is_valid():
+            thread = form.save(commit=False)
+            thread.author = request.user
+            thread.save()
+            return redirect('forum_home')
+    else:
+        form = ThreadForm()
+    return render(request, 'forum/create_thread.html', {'form': form})
